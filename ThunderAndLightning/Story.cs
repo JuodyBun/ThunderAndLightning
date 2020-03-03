19cfa68ae6e22a50f9bbbaf9c3184a34301fee5e
Juody Bun <bjuody@gmail.com>
2020-03-03 17:19:08 -0600

    added needed changes comments

--- ThunderAndLightning/Story.cs
+++ ThunderAndLightning/Story.cs
@@ -4,5 +4,17 @@ using System.Text;
 
 namespace ThunderAndLightning
 {
-    
+    public class Introduction
+    {
+        public string name;
+        public string response;
+        public string GetName()
+        {
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine().ToUpper();
+            Console.WriteLine($"{name}, is that correct? Type 'NO' to re-enter your name or press 'Enter' to continue the story.");
+            response = Console.ReadLine().ToUpper();
+            return GetName();
+        }
+    }
 }
